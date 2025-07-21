4197e6a951496fe9c068ea12b5dc4e6f5cb2c60a
Radik <h2xdeveloper@gmail.com>
2025-07-21 13:00:14 +0400

    Light: Remove polygon processing method

--- core/light.js
+++ core/light.js
@@ -12,31 +12,4 @@ export class PointLight extends GNode3D {
     this.radius = radius;
     this.energy = energy;
   }
-
-  /**
-    * @param { import("./camera3d").Camera3D } camera
-    * @param { import("./polygon.js").Polygon } polygon
-    * @param { Color } inColor
-    */
-  processPolygon(camera, polygon, inColor) {
-    const lightPos = this.transform.position
-      .applyTransform(this.globalTransform)
-      .applyTransform(camera.transform.inverse);
-
-    const delta1 = lightPos.sub(polygon.v1)
-    const delta2 = lightPos.sub(polygon.v2);
-    const delta3 = lightPos.sub(polygon.v3);
-    const delta = lightPos.sub(polygon.center);
-
-    const lightDir = delta.normalized;
-
-    const distance = Math.min(delta1.length, delta2.length, delta3.length, delta.length);
-    const percent = Math.max(0, 1 - distance / this.radius);
-
-    if (percent <= 0) return inColor;
-
-    const lightShining = Math.pow(Math.max(0, polygon.normal.dot(lightDir)), 1) * percent;
-
-    return inColor.add(this.color.mul(lightShining * this.energy));
-  }
 }
